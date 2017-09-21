03c69a36be160ee8ee370d7d9f8195b3bcf0afcf
HQuser <abdurvsrahman@gmail.com>
2017-09-21 21:09:22 +0500

    Add Restaurants

--- app/src/main/java/com/example/abdur/lahorelocations/RestaurantActivity.java
+++ app/src/main/java/com/example/abdur/lahorelocations/RestaurantActivity.java
@@ -2,6 +2,9 @@ package com.example.abdur.lahorelocations;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Abdur on 21-Sep-17.
@@ -11,8 +14,17 @@ public class RestaurantActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_view);
+
+        // List of locations
+        ArrayList<Location> locations = new ArrayList<>();
+        locations.add(new Location("Tabaq Restaurant", R.drawable.tabaq_restaurant, "Brunch, Lunch, Dinner, Accepts Mastercard, Accepts Visa, Buffet, Delivery, Highchairs"));
+        locations.add(new Location("Casa Restaurant", R.drawable.casa_restaurant, "With an ambience of Victorian England and a highly unique cuisine that is bound to get you salivating, Lahore’s new restaurant, Casa Bianca"));
+        locations.add(new Location("Ginoy Restaurant", R.drawable.ginsoy_restaurant, "Ginsoy is a contemporary fine dining Chinese restaurant that serves flavorful Chinese cuisine in a sleek and colorful environment."));
 
 
+        // Setting up ListView and adding adapter
+        ListView listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(new LocationsAdapter(this, locations));
     }
 }
